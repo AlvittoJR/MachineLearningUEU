598e69e2c63e4a8c8b41941d206851aff1554969
AlvittoJR <136164276+AlvittoJR@users.noreply.github.com>
2023-11-15 04:02:56 +0700

    Add files via upload

---
+++ UTS ML/ESSAY/Essay.py
@@ -0,0 +1,31 @@
+SOAL
+1) Jelaskan apa yang dimaksud dengan Machine Learning
+2) Berikan contoh penerapan implemetasi dalam kehidupan sehari-hari terkait
+Machine Learning. Dan jelaskan mengapa membutuhkannya dan manfaatnya
+3) Jelaskan macam-macam taxonomi dalam pengerapan Machine Learning
+
+JAWABAN
+1. Machine Learning (Pembelajaran Mesin):
+Machine Learning (ML) adalah cabang dari kecerdasan buatan (AI) yang fokus pada pengembangan sistem yang mampu belajar dari data. Sebuah sistem machine learning menggunakan algoritma dan model matematis untuk menganalisis data, mengenali pola, dan membuat keputusan tanpa harus secara eksplisit diprogram untuk tugas tersebut. Dengan kata lain, mesin dapat belajar dari pengalaman dan meningkatkan kinerjanya seiring waktu.
+2. Contoh Penerapan dan Manfaat Machine Learning dalam Kehidupan Sehari-hari:
+Contoh Penerapan:
+•	Pengenal Wajah (Face Recognition): Digunakan dalam smartphone untuk membuka kunci perangkat atau di bandara untuk identifikasi keamanan.
+•	Rekomendasi Produk (Product Recommendations): Platform e-commerce seperti Amazon menggunakan ML untuk memberikan rekomendasi produk berdasarkan riwayat pembelian dan preferensi pengguna.
+•	Pengenalan Suara (Speech Recognition): Asisten virtual seperti Siri atau Google Assistant menggunakan ML untuk mengenali dan memahami perintah suara.
+•	Mobil Otonom (Autonomous Vehicles): Machine Learning digunakan dalam pengembangan mobil otonom untuk mengenali objek di sekitarnya dan membuat keputusan berdasarkan data sensor.
+Manfaat:
+•	Peningkatan Efisiensi: ML dapat memproses dan menganalisis data dalam skala besar dengan cepat, memungkinkan pengambilan keputusan yang lebih efisien.
+•	Personalisasi: Dengan menganalisis pola perilaku pengguna, ML dapat memberikan pengalaman yang lebih personal, seperti rekomendasi yang disesuaikan dan konten yang relevan.
+•	Prediksi yang Akurat: Dengan menggunakan model ML, kita dapat membuat prediksi yang lebih akurat berdasarkan pola data historis.
+•	Automatisasi Tugas: ML dapat digunakan untuk mengotomatisasi tugas-tugas yang repetitif dan memakan waktu.
+3. Macam-macam Taxonomi dalam Penerapan Machine Learning:
+Berdasarkan Tujuan Pembelajaran:
+•	Supervised Learning (Pembelajaran Terawasi): Model dilatih menggunakan data yang sudah memiliki label, dengan tujuan untuk membuat prediksi atau mengidentifikasi pola.
+•	Unsupervised Learning (Pembelajaran Tanpa Pengawasan): Model mencoba mengidentifikasi pola atau struktur dalam data tanpa label. Contoh termasuk clustering dan reduksi dimensi.
+Berdasarkan Jenis Algoritma:
+•	Regression: Digunakan untuk memprediksi nilai numerik berdasarkan data yang telah diamati sebelumnya.
+•	Classification: Digunakan untuk mengklasifikasikan objek ke dalam kelas atau kategori tertentu.
+•	Clustering: Memisahkan data ke dalam kelompok-kelompok berdasarkan kesamaan karakteristik.
+Berdasarkan Pendekatan Pembelajaran:
+•	Batch Learning: Model dilatih menggunakan seluruh set data sekaligus.
+•	Online Learning: Model diperbarui secara kontinu saat data baru tiba.
